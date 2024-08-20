dc76841999a728564a51282c582430d83353e882
gardening-y <yj4185@duksung.ac.kr>
2024-08-20 22:16:15 +0900

    [FEAT] report router 연결

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from .database import engine
 from . import models
-from .endpoints import users, posts, potholes
+from .endpoints import users, posts, potholes, reports
 import os
 from fastapi import FastAPI, File, UploadFile
 from fastapi.responses import JSONResponse
@@ -10,6 +10,7 @@ models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
-app.include_router(users.router, prefix="/users", tags=["users"])
-app.include_router(posts.router, prefix="/posts", tags=["posts"])
-app.include_router(potholes.router, prefix="/potholes", tags=["potholes"])
+app.include_router(users.router, prefix="/user", tags=["user"])
+app.include_router(reports.router, prefix="/report", tags=["report"])
+app.include_router(posts.router, prefix="/post", tags=["post"])
+app.include_router(potholes.router, prefix="/pothole", tags=["pothole"])
