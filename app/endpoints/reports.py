493e0a266065860c602fd6188c60722cb3921583
gardening-y <yj4185@duksung.ac.kr>
2024-10-04 20:23:04 +0900

    [FEAT] Report 수정시 문자 발송 로직 구현

--- app/endpoints/reports.py
+++ app/endpoints/reports.py
@@ -3,6 +3,8 @@ from sqlalchemy.orm import Session
 from typing import List
 from .. import crud, schemas
 from ..database import SessionLocal
+from jmunja import smssend
+
 
 router = APIRouter()
 
@@ -34,3 +36,20 @@ def delete_report(report_id: int, db: Session = Depends(get_db)):
     if db_report is None:
         raise HTTPException(status_code=404, detail="Report not found")
     return db_report
+
+uid = "portrancapstone"
+upw = "34eb0a89dcbf268691b116c2b11704"
+subject = "[Fortran] 포트홀 신고 관련 안내"
+content = "수동 등록하신 포트홀이 처리 완료되었습니다. 앱을 통해 확인해주세요."
+@router.put("/{report_id}", response_model=schemas.Report)
+def update_report(report_id: int, report: schemas.ReportUpdate, sms_request: schemas.SMSRequest, db: Session = Depends(get_db)):
+    db_report = crud.get_report(db, report_id=report_id)
+    if db_report is None:
+        raise HTTPException(status_code=404, detail="Report not found")
+    jphone = smssend.JmunjaPhone(uid, upw)
+    jphone.send(subject, content, sms_request.hpno)
+    jweb = smssend.JmunjaWeb(uid, upw)
+    jweb.send(subject, content, sms_request.hpno, sms_request.hpno)
+
+    return crud.update_report_done(db=db, report_id=report_id, done=report.done)
+
