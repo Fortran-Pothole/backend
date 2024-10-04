6427890dab5b47a20b905f52db17b06b8ff3f468
gardening-y <yj4185@duksung.ac.kr>
2024-10-04 20:22:37 +0900

    [FEAT] Potholes 시간필드, 문자 발송 로직 구현

--- app/endpoints/potholes.py
+++ app/endpoints/potholes.py
@@ -2,6 +2,13 @@ from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from .. import crud, schemas
 from ..database import SessionLocal
+from jmunja import smssend
+
+
+uid = "portrancapstone"
+upw = "34eb0a89dcbf268691b116c2b11704"
+subject = "[Fortran] 포트홀 신고 관련 안내"
+content = "등록하신 포트홀이 처리 완료되었습니다. 앱을 통해 확인해주세요."
 
 router = APIRouter()
 
@@ -26,17 +33,21 @@ def read_potholes(pothole_id: int, db: Session = Depends(get_db)):
     potholes = crud.get_pothole(db, pothole_id)
     return potholes
 
-
 @router.get("/done/{done}", response_model=list[schemas.Pothole])
 def read_potholes_done(done: int, db: Session = Depends(get_db)):
     potholes = crud.get_potholes_done(db, done)
     return potholes
 
 @router.put("/{pothole_id}", response_model=schemas.Pothole)
-def update_pothole(pothole_id: int, pothole: schemas.PotholeUpdate, db: Session = Depends(get_db)):
+def update_pothole(pothole_id: int, pothole: schemas.PotholeUpdate, sms_request: schemas.SMSRequest, db: Session = Depends(get_db)):
     db_pothole = crud.get_pothole(db, pothole_id=pothole_id)
     if db_pothole is None:
         raise HTTPException(status_code=404, detail="Pothole not found")
+    jphone = smssend.JmunjaPhone(uid, upw)
+    jphone.send(subject, content, sms_request.hpno)
+    jweb = smssend.JmunjaWeb(uid, upw)
+    jweb.send(subject, content, sms_request.hpno, sms_request.hpno)
+
     return crud.update_pothole(db=db, pothole_id=pothole_id, pothole=pothole)
 
 
