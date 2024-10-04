45e6a73628955bad105c2873412883560d0ab6f4
gardening-y <yj4185@duksung.ac.kr>
2024-10-04 20:21:48 +0900

    [FEAT] User 번호필드 추가

--- app/models.py
+++ app/models.py
@@ -9,6 +9,7 @@ class User(Base):
     id = Column(Integer, primary_key=True, index=True, autoincrement=True)  # id는 자동 생성
     name = Column(String, index=True)
     password = Column(String, nullable=False)
+    phone = Column(String, nullable=False)  # 전화번호 필드 추가
     reports = relationship("Report", back_populates="owner")
 
 class Pothole(Base):
@@ -23,6 +24,7 @@ class Pothole(Base):
                     # 처리 완료 1
     report_count = Column(Integer, default=0)  # 동일한 위치의 Report 수를 저장하는 필드
     warning = Column(Integer, default=lambda: random.randint(1, 5))  # 1~5 사이의 랜덤 정수
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
     posts = relationship("Post", cascade="all, delete-orphan", back_populates="pothole")
 
     def __init__(self, **kwargs):
@@ -51,7 +53,11 @@ class Report(Base):
     user_id = Column(Integer, ForeignKey("users.id"))
     location = Column(String, nullable=False)
     content = Column(Text, nullable=False)
-    images = Column(Text)
+    done = Column(Integer, default=-1)
+                    # 처리 전 -1
+                    # 처리 중 0
+                    # 처리 완료 1
+    images = Column(Text, default="https://fortran-pothole.s3.ap-northeast-2.amazonaws.com/V3F_HY_1562_20160212_013414_N_CH2_Seoul_Sun_Highway_Day_84497.png")
     created_at = Column(DateTime(timezone=True), server_default=func.now())
     owner = relationship("User", back_populates="reports")
     # notice_count = 
