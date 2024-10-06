8e716a941c1f02fa8e76ff3415b13e9ba89266e3
gardening-y <yj4185@duksung.ac.kr>
2024-10-06 22:59:15 +0900

    [FIX] DB연결

--- app/database.py
+++ app/database.py
@@ -1,6 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
+
 import os
 
 # SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"
@@ -8,8 +10,13 @@ import os
 # engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
 # SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 # Base = declarative_base()
+load_dotenv()
+
 
 DATABASE_URL = os.getenv("DATABASE_URL")
+
+if not DATABASE_URL:
+    raise ValueError("DATABASE_URL 환경 변수가 설정되지 않았습니다.")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
